a05c385abbe66482d5d4095a89e784f0286224b2
mtdp <475166372@qq.com>
2020-02-17 15:08:05 +0800

    2020.2.17

---
+++ src/jmy1_19/GitCommit2_17.java
@@ -0,0 +1,4 @@
+package jmy1_19;
+
+public class GitCommit2_17 {
+}
--- src/jmy1_19/Hello1_19.java
+++ src/jmy1_19/Hello1_19.java
@@ -3,5 +3,6 @@ package jmy1_19;
 public class Hello1_19 {
     public static void main(String[] args) {
         System.out.println("hello world 1.19!");
+        System.out.println("hello world 2.17!");
     }
 }
