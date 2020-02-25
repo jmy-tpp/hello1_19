034933b4ec412fff5a1133c737c1f38ce8167da4
mtdp <475166372@qq.com>
2020-02-25 18:03:27 +0800

    2020.1.19

--- src/jmy1_19/GitCommit2_17.java
+++ src/jmy1_19/GitCommit2_17.java
@@ -1,5 +1,7 @@
 package jmy1_19;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 public class GitCommit2_17 {
 
 }
--- src/jmy1_19/Hello1_19.java
+++ src/jmy1_19/Hello1_19.java
@@ -2,7 +2,11 @@ package jmy1_19;
 
 public class Hello1_19 {
     public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(i);
+        }
         System.out.println("hello world 1.19!");
         System.out.println("hello world 2.17!");
+
     }
 }
