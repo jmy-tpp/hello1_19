2bf7b38172a1e57aac471a1fb3e0080d2dc15078
mtdp <475166372@qq.com>
2020-02-28 23:56:38 +0800

    2020.1.19

--- src/jmy1_19/Hello1_19.java
+++ src/jmy1_19/Hello1_19.java
@@ -4,4 +4,5 @@ public class Hello1_19 {
     public static void main(String[] args) {
         System.out.println("hello world 1.19!");
     }
+
 }
---
+++ src/jmy1_19/add.java
@@ -0,0 +1,7 @@
+package jmy1_19;
+
+public class add {
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
