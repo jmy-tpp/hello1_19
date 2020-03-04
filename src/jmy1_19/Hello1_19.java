31df5e146f18acbaac41913f4d2ce7f52ec27a1c
贾梦瑶 <jiamengyao02@meituan.com>
2020-03-04 22:27:45 +0800

    2020.1.19

--- src/jmy1_19/Hello1_19.java
+++ src/jmy1_19/Hello1_19.java
@@ -1,8 +1,11 @@
 package jmy1_19;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
 public class Hello1_19 {
     public static void main(String[] args) {
         System.out.println("hello world 1.19!");
+        System.out.println("3.4，要测试");
     }
 
 }
